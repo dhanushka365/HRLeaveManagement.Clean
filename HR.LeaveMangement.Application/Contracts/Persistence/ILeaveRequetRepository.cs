cb8f233365bb2ce282f9b7b8a4adb241eb44c69e
dhanushka365 <pasindudhanushka365@gmail.com>
2024-09-07 09:20:32 +0530

    chnages committed

---
+++ HR.LeaveMangement.Application/Contracts/Persistence/ILeaveRequetRepository.cs
@@ -0,0 +1,10 @@
+﻿using HR.LeaveManagement.Domain;
+
+namespace HR.LeaveManagement.Application.Contracts.Persistence
+{
+    public interface ILeaveRequetRepository : IGenericRepository<LeaveRequest>
+    {
+
+
+    }
+}
---
+++ HR.LeaveMangement.Application/Features/LeaveType/Queries/GetAllLeaveTypes/LeaveTypeDto.cs
@@ -0,0 +1,9 @@
+﻿namespace HR.LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes
+{
+    public class LeaveTypeDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DefaultDays { get; set; }
+    }
+}
